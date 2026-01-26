11bae1f97bb039f34376d1113607801f158ed368
RuslanKabachok <ruslan.kabachok@gmail.com>
2026-01-26 18:36:27 +0200

    Finished TaskList

--- live-coding/src/ReactTraining/Task/Task.jsx
+++ live-coding/src/ReactTraining/Task/Task.jsx
@@ -1,14 +1,10 @@
 import './Task.css';
 
-function Task({ title, descr, isCompleted }) {
-
-console.log({title, descr, isCompleted});
-
+function Task({ title, descr, isCompleted, children }) {
   return (
     <div
-      className={`${
-        isCompleted ? "bg-blue-200" : "bg-red-200"
-      } flex flex-col items-center gap-3 p-6 shadow-lg rounded-xl m-4 transition duration-300 hover:scale-105`}
+      className={`${isCompleted ? "bg-blue-200" : "bg-red-200"
+        } flex flex-col items-center gap-3 p-6 shadow-lg rounded-xl m-4 transition duration-300 hover:scale-105`}
     >
       <h1 className="text-2xl font-semibold text-gray-800">{title}</h1>
       <p className="text-gray-600 italic text-center">{descr}</p>
@@ -17,6 +13,7 @@ console.log({title, descr, isCompleted});
       ) : (
         <span className="font-medium text-red-600">❌ Task not completed</span>
       )}
+      {children}
     </div>
   );
 }
---
+++ live-coding/src/ReactTraining/TaskList/TaskList.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import { nanoid } from 'nanoid';
+import Task from '../Task/Task';
+import '../Task/Task.css'
+
+export default function TaskList() {
+    const [tasks, setTasks] = useState([])
+    const [value, setValue] = useState('')
+    const [descr, setDescr] = useState('')
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if (!value) return
+
+        const newTask = { text: value, checked: false, id: nanoid(), descr: descr }
+        setTasks(prevTasks => [...prevTasks, newTask]);
+        setValue('');
+        setDescr('');
+    };
+
+    const toggleTask = (id) => {
+        const result = tasks.map(task => {
+            if (task.id === id) {
+                return { ...task, checked: !task.checked }
+            } return task
+        })
+        setTasks(result)
+    }
+
+
+    return <div className='flex flex-col items-center min-h-screen bg-gray-100 p-10'>
+        <form onSubmit={onSubmit} className='flex gap-4 mb-6'>
+            <input placeholder='Enter task' type="text" name='task' id='task' className='px-4 py-2 rounded-xl border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400' value={value} onChange={(e) => { setValue(e.target.value) }} />
+            <input placeholder='Enter task description' type="text" name='task' id='task' className='px-4 py-2 rounded-xl border shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400' value={descr} onChange={(e) => { setDescr(e.target.value) }} />
+            <button className='px-6 py-2 rounded-xl bg-blue-500 text-white font-medium hover:bg-blue-600 transition' type='submit'>Add task</button>
+        </form>
+        {tasks.length ? <h1 className='text-xl font-semibold mb-4'>TaskList: </h1> : null}
+        <ul className='w-full space-y-3'>{tasks.map(task =>
+            <Task key={task.id} title={task.text} isCompleted={task.checked} descr={task.descr} children={<input type="checkbox" name="isCompleted" id='check' checked={task.checked} onChange={() => toggleTask(task.id)} />}>
+            </Task>)}
+        </ul>
+    </div>
+}
